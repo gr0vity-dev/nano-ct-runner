3c7e1bcdd446994941d238552cae62d6422dc014
gr0vity-dev <iq.cc@pm.me>
2023-04-18 15:02:52 +0200

    poc

---
+++ ci/parse_config.py
@@ -0,0 +1,20 @@
+import json
+import sys
+
+
+def main():
+    key = sys.argv[1]
+
+    with open("ci/config.json") as config_file:
+        config = json.load(config_file)
+
+    if key == "user_id":
+        print(config["user_id"])
+    elif key == "suite_id":
+        print(config["suite_id"])
+    else:
+        print(json.dumps(config["testcases"]))
+
+
+if __name__ == "__main__":
+    main()
